72b2d7596b3ca8e3cf22d6c107848e8dc3a34ed7
godrozhdenia <70814997+godrozhdenia@users.noreply.github.com>
2024-08-18 17:17:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const song = new Audio('cool song.mp3');
+const container = document.getElementById('container')
+const vaxxed = document.getElementById('vaxxed')
+function access(parameter){
+    if (parameter == 'granted'){
+        vaxxed.style.visibility = "hidden";
+        container.style.visibility = "visible";
+        song.play()
+        song.loop = true;
+    }
+    else{
+        alert('buy 💉🧬 and come back later!')
+        setTimeout(function(){window.location.href = "https://www.unicorn.meme/";}, 1000)
+        
+    }
+}
